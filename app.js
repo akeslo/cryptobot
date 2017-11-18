63872a0c07e18db12880bddce042c6eee8a710f6
Akeslo <akeslo@kesdev.space>
2017-11-17 23:45:15 -0500

    update

--- app.js
+++ app.js
@@ -279,8 +279,8 @@ bot.on('ready', () => {
             channel.send(saySuccessMessage("I set the update channel to " + channel.name + ". That's where you'll get updated automatically from now on."));
           } else if (message.includes("interval")) {
             parseFloatComplex(message).then(function(num){
+              channel.send("here");
               setUpdateInterval(num).then(function(resolved){
-                channel.send("here");
                 channel.send(saySuccessMessage("You'll be automagically updated on your coins interests every " + num + " hours from now on."));
               }).catch(function(err){
                 payload.reply(err);
